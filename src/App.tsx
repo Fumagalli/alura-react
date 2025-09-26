938faebe8a5a08bf27e3d34929c2dab0c743adaa
Ângelo Fumagalli <fumagalli.angelo@gmail.com>
2025-09-26 17:37:20 -0300

    chore: add form fields as components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,51 @@
+import type { JSX } from 'react/jsx-runtime';
 import './App.css'
+import type { ClassAttributes, HTMLAttributes, ReactNode, LabelHTMLAttributes, InputHTMLAttributes } from 'react';
+
+function FormTitle(props: JSX.IntrinsicAttributes & ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement>) {
+  return (
+    <h2 {...props}>
+      {props.children}
+    </h2>
+  )
+}
+
+function FormField({ children }: { children: ReactNode }) {
+  return (
+    <fieldset>
+      {children}
+    </fieldset>
+  )
+}
+
+function FormLabel(props: JSX.IntrinsicAttributes & LabelHTMLAttributes<HTMLLabelElement>) {
+  return (
+    <label {...props}>
+      {props.children}
+    </label>
+  )
+}
+
+function FormInput(props: JSX.IntrinsicAttributes & InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input {...props} />
+  )
+}
 
 function FormEventCreate() {
   return (
     <form className='form-event-create'>
-      <h2>Preencha para criar um evento:</h2>
-      <fieldset>
-        <label htmlFor="name">
-          Qual o nome do evento?
-        </label>
+      <FormTitle>
+        Preencha para criar um evento:
+      </FormTitle>
 
-        <input type="text" id="name" placeholder='Summer dev hits' />
-      </fieldset>
+      <FormField>
+        <FormLabel htmlFor="name">
+          Qual o nome do evento?
+        </FormLabel>
+
+        <FormInput type="text" id="name" placeholder='Summer dev hits' />
+      </FormField>
     </form>
   )
 }
